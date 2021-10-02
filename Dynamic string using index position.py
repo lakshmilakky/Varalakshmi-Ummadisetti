9e0f239c8c52bf888d9f2f28cd1f63fd959ecf4c
lakshmilakky <varalakshmi1472000@gmail.com>
2021-10-02 11:29:07 +0530

    Create Dynamic string using index position.py

---
+++ Dynamic string using index position.py
@@ -0,0 +1,5 @@
+person = input ("Enter Person Name:")
+movie_name = input ("Enter Movie Name:") 
+theatre_name = input ("Enter Theatre Name:")
+txt = "{0} went to {1} movie at {2} theatre"
+print(txt.format(person,movie_name, theatre_name)) 
